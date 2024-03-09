f49d58d802554c21d4c1b67b6e189e7c2165ec45
Ommaker <108610817+Ommaker@users.noreply.github.com>
2024-03-09 14:58:58 +0530

    Add files via upload

---
+++ details.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: '7051728380:AAHUx8f9PVe9loe9VgYAy2TMZeiJO2xw92c',
+    admins: [6619405270],
+    mongo_url: 'mongodb+srv://utsavkumar:utsavpass@cluster0.frrcq2l.mongodb.net/',
+}
+
